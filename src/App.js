169e049f58ac879b8e1343c66c1bfa43ed6dce04
Carlton Joseph <carlton.joseph@gmail.com>
2019-11-03 15:05:43 -0800

    update doc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      Create react app, to github page, and deploying with travis.
+      Create react app, to github page, and deploying with travis. Update doc to
+      see if travis auto built.
     </div>
   );
 }
